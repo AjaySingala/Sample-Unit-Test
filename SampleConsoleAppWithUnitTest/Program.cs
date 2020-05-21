2f3d22310792c04536775381cffd9902caf762eb
AjaySingala <ajay.singala@gmail.com>
2020-05-21 16:39:23 +0530

    First commit

---
+++ SampleConsoleAppWithUnitTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SampleConsoleAppWithUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svc = new PrimeService();
+            for (int i = -5; i < 100; i++)
+            {
+                var result = (svc.IsPrime(i)) ? true : false;
+                Console.WriteLine($"{i} is a Prime? {result}");
+            }
+        }
+    }
+}
